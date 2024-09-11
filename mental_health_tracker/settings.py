9212810d210d33653b0d0cfde499f7458fa19473
Muhammad Afwan Hafizh <mirananightfall228@gmail.com>
2024-09-11 14:49:01 +0700

    tutorial 2 done

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -54,9 +54,9 @@ ROOT_URLCONF = "mental_health_tracker.urls"
 
 TEMPLATES = [
     {
-        "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
-        "APP_DIRS": True,
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [BASE_DIR / 'templates'], # Tambahkan konten baris ini
+        'APP_DIRS': True,
         "OPTIONS": {
             "context_processors": [
                 "django.template.context_processors.debug",
@@ -122,3 +122,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://muhammad-afwan-mentalhealthtracker.pbp.cs.ui.ac.id", "https://muhammad-afwan-mentalhealthtracker.pbp.cs.ui.ac.id"]
